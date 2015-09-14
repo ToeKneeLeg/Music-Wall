2a7b28af8a0512222585236310c80f1f9ae43933
Tony Lei <tonykclei@gmail.com>
2015-09-14 22:01:15 +0000

    Added functionalities

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,30 @@
 get '/' do
   erb :index
 end
+
+get '/tracks/new' do
+  erb :'tracks/new'
+end
+
+get '/tracks/:id' do
+  @tracks = Track.find params[:id]
+  erb :'tracks/show'
+end
+
+get '/tracks' do
+  @tracks = Track.all
+  erb :'tracks/index'
+end
+
+post '/tracks' do 
+  @tracks = Track.new(
+    title: params[:title],
+    author: params[:author],
+    url: params[:url]
+    )
+  if @tracks.save
+    redirect '/tracks'
+  else
+    erb :'tracks/new'
+  end
+end
---
+++ app/models/track.rb
@@ -0,0 +1,5 @@
+class Track < ActiveRecord::Base
+
+  validates :title, :author, presence: true
+
+end
